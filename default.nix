853098443f53a8d75cd26a424ad4e5f2b2faa2e0
Adithya Kumar <adithya@composewell.com>
2022-03-10 16:22:39 +0530

    Add shell-ish packages to the env

--- default.nix
+++ default.nix
@@ -20,11 +20,38 @@ let haskellPackages =
                     then orig.overrideAttrs (oldAttrs: { src = null; })
                     else orig;
 
+  mkPkgGit = super: gitSrc: ref: name:
+    let
+      src = builtins.fetchGit {
+        url = gitSrc;
+        ref = ref;
+      };
+    in super.callCabal2nix name src { };
+
     mkHaskellPackages = inShell:
         haskellPackages.override {
             overrides = self: super:
                 with nixpkgs.haskell.lib;
                 {
+          streamly-coreutils = let
+            src = "git@github.com:composewell/streamly-coreutils.git";
+            ref = "a12756efe24bbf303f2f6d66a95426c7657d297b";
+          in mkPkgGit super src ref "streamly-coreutils";
+          streamly-shell = let
+            src = "https://github.com/composewell/streamly-shell.git";
+            ref = "7d77ddc0c06aec21fbbfd1b920ec4f58da654a1b";
+          in mkPkgGit super src ref "streamly-shell";
+          streamly-process = nixpkgs.haskell.lib.dontCheck
+            (super.callHackageDirect {
+              pkg = "streamly-process";
+              ver = "0.1.0";
+              sha256 = "01nxisqfmn29fbirdsx71sfjp2rdqwrf469qyjcql2d11i1bxn94";
+            } { });
+          streamly = super.callHackageDirect {
+            pkg = "streamly";
+            ver = "0.8.0";
+            sha256 = "0vy2lkljizlhpbpbybmg9jcmj2g4s1aaqd2dzy5c0y0n4rgwxask";
+          } { };
                     bench-show = super.callHackageDirect {
                       pkg = "bench-show";
                       ver = "0.3.2";
