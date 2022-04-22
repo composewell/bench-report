7c452971fd41707aa7f23795fce802cc59e92fc8
Harendra Kumar <harendra@composewell.com>
2022-04-22 12:59:10 +0530

    Refactor, use dependency order, add docs

--- lib/Utils/QuasiQuoter.hs
+++ lib/Utils/QuasiQuoter.hs
@@ -1,8 +1,13 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
--- | Description: Template Haskell code. This code needs to be in a separate
+-- | String interpolation.
+--
+-- Template Haskell code. This code needs to be in a separate
 -- module because of GHC stage restriction.
-
+--
+-- XXX Rename the module to StringInterpolation. Also, move this to an
+-- appropriate package.
+--
 -- Some of the code in this module is inspired by neat-interpolation by
 -- nikita-volkov.
 
@@ -15,103 +20,18 @@ module Utils.QuasiQuoter
 -- Imports
 --------------------------------------------------------------------------------
 
-import Control.Monad (void)
 import Control.Applicative (Alternative(..))
+import Control.Monad (void)
 import Control.Monad.Catch (MonadCatch)
 import Data.Char (isAlphaNum)
 import Streamly.Internal.Data.Parser (Parser)
 
-import Language.Haskell.TH.Quote
 import Language.Haskell.TH
+import Language.Haskell.TH.Quote
 
-import qualified Streamly.Internal.Data.Stream.IsStream as Stream
-import qualified Streamly.Internal.Data.Parser as Parser
 import qualified Streamly.Internal.Data.Fold as Fold
-
---------------------------------------------------------------------------------
--- Types
---------------------------------------------------------------------------------
-
-type Line = [LineContent]
-
-data LineContent
-    = LineContentText String
-    | LineContentIdentifier String
-    deriving (Show, Eq)
-
---------------------------------------------------------------------------------
--- Functions
---------------------------------------------------------------------------------
-
-replace :: Eq a => a -> a -> [a] -> [a]
-replace i j = map replaceF
-
-    where
-
-    replaceF x
-        | x == i = j
-        | otherwise = x
-
--- We can merge line and multi by making parsing smarter
-line :: QuasiQuoter
-line =
-    QuasiQuoter
-        { quoteExp = smartStringE (replace '\n' ' ' . stripAndPad)
-        , quotePat = notSupported
-        , quoteType = notSupported
-        , quoteDec = notSupported
-        }
-
-    where
-
-    notSupported = error "line: Not supported."
-
-multi :: QuasiQuoter
-multi =
-    QuasiQuoter
-        { quoteExp = smartStringE (++ " ")
-        , quotePat = notSupported
-        , quoteType = notSupported
-        , quoteDec = notSupported
-        }
-
-    where
-
-    notSupported = error "multi: Not supported."
-
--- | Clear all new lines and trim the input
--- This has a small workaround for parser alternative instance bug
--- We add a space in the end
-stripAndPad :: String -> String
-stripAndPad =
-    reverse
-        . (' ' :)
-        . dropWhile isSpaceOrNewLine . reverse . dropWhile isSpaceOrNewLine
-
-    where
-
-    isSpaceOrNewLine x = x == ' ' || x == '\n'
-
-smartStringE :: (String -> String) -> String -> Q Exp
-smartStringE preprocess ln =
-    case Stream.parse lineParser (Stream.fromList (preprocess ln)) of
-        Left _ -> fail "Some error has occured."
-        Right xs ->
-            -- We need remove the ' ' at the end that we add for the hack.
-            if last xs == LineContentText " "
-            then lineExp (init xs)
-            else lineExp xs
-
-lineExp :: Line -> Q Exp
-lineExp xs = appE [| concat |] $ listE $ map contentExp xs
-
-contentExp :: LineContent -> Q Exp
-contentExp (LineContentText text) = stringE text
-contentExp (LineContentIdentifier name) = do
-    valueName <- lookupValueName name
-    case valueName of
-        Just vn -> varE vn
-        Nothing -> fail $ "Value `" ++ name ++ "` is not in scope"
+import qualified Streamly.Internal.Data.Parser as Parser
+import qualified Streamly.Internal.Data.Stream.IsStream as Stream
 
 --------------------------------------------------------------------------------
 -- Parsing
@@ -127,18 +47,32 @@ charP c = Parser.satisfy (== c)
 alphaNumP :: MonadCatch m => Parser m Char Char
 alphaNumP = Parser.satisfy isAlphaNum
 
+type Line = [LineContent]
+
+data LineContent
+    = LineContentText String
+    | LineContentIdentifier String
+    deriving (Show, Eq)
+
 lineParser :: MonadCatch m => Parser m Char Line
 lineParser = Parser.many content Fold.toList
 
     where
 
+    -- Reference to a haskell symbol, starts with a $ symbol followed by the
+    -- Haskell identifier.
     identifierSimple =
         Parser.some (alphaNumP <|> charP '\'' <|> charP '_') Fold.toList
     identifierInBraces = charP '{' *> identifierSimple <* charP '}'
     identifier =
         fmap LineContentIdentifier
             $ charP '$' *> (identifierInBraces <|> identifierSimple)
+
+    -- A "$$" is parsed as a single "$"
     escapedDollar = fmap (LineContentText . (: [])) $ charP '$' *> charP '$'
+
+    -- Anything other than an identifier or a $$ is regular text
+    --
     -- "Parser.count" is undefined. The current implementation eats the
     -- malformed '$' instead of erroring out. This should be fixed if 'count' is
     -- used.
@@ -151,4 +85,97 @@ lineParser = Parser.many content Fold.toList
             <|> void (Parser.lookAhead identifier)
             <|> Parser.eof
     contentText = LineContentText <$> Parser.manyTill anySingle end Fold.toList
+
     content = escapedDollar <|> identifier <|> contentText
+
+contentExp :: LineContent -> Q Exp
+contentExp (LineContentText text) = stringE text
+contentExp (LineContentIdentifier name) = do
+    valueName <- lookupValueName name
+    case valueName of
+        Just vn -> varE vn
+        Nothing ->
+            fail
+                $ "Interpolated string: Haskell symbol `" ++ name
+                ++ "` is not in scope"
+
+lineExp :: Line -> Q Exp
+lineExp xs = appE [| concat |] $ listE $ map contentExp xs
+
+smartStringE :: String -> Q Exp
+smartStringE ln =
+    case Stream.parse lineParser (Stream.fromList ln) of
+        Left _ -> fail "Parsing of interpolated string failed."
+        Right xs ->
+            -- We need to remove the ' ' at the end that we add for the hack.
+            lineExp
+                $ if last xs == LineContentText " "
+                  then init xs
+                  else xs
+
+--------------------------------------------------------------------------------
+-- Single line string
+--------------------------------------------------------------------------------
+
+-- | Replace a value with another in a list.
+replace :: Eq a => a -> a -> [a] -> [a]
+replace i j = map replaceF
+
+    where
+
+    replaceF x
+        | x == i = j
+        | otherwise = x
+
+-- | Strip contiguous space and newline characters at the beginning and at the
+-- end of the string.
+--
+-- Add a space at the end to workaround the parser alternative instance bug
+stripAndPad :: String -> String
+stripAndPad =
+    reverse
+        . (' ' :)
+        . dropWhile isSpaceOrNewLine . reverse . dropWhile isSpaceOrNewLine
+
+    where
+
+    -- XXX Should we use isSpace instead?
+    isSpaceOrNewLine x = x == ' ' || x == '\n'
+
+-- We can merge line and multi by making parsing smarter
+--
+-- | A QuasiQuoter that treats the input as a single line string (replaces
+-- newlines in the input by spaces), and replaces any @$symbol@ by the value of
+-- the Haskell symbol @symbol@ which is in scope. Any leading or trailing
+-- spaces are removed.
+line :: QuasiQuoter
+line =
+    QuasiQuoter
+        { quoteExp = smartStringE . replace '\n' ' ' . stripAndPad
+        , quotePat = notSupported
+        , quoteType = notSupported
+        , quoteDec = notSupported
+        }
+
+    where
+
+    notSupported = error "line: Not supported."
+
+--------------------------------------------------------------------------------
+-- Multi line string
+--------------------------------------------------------------------------------
+
+-- | Like 'line' but treats the input as a multiline string any newline
+-- characters in the input are retained as is.
+multi :: QuasiQuoter
+multi =
+    QuasiQuoter
+        { quoteExp = smartStringE . (++ " ")
+        , quotePat = notSupported
+        , quoteType = notSupported
+        , quoteDec = notSupported
+        }
+
+    where
+
+    notSupported = error "multi: Not supported."
