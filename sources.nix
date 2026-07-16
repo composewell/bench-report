1b58d5311cb7417d2b9243e3bd1840c58f837cc6
Harendra Kumar <harendra@composewell.com>
2026-07-17 02:32:53 +0530

    Pin streamly-coreutils to latest master commit

--- sources.nix
+++ sources.nix
@@ -1,5 +1,8 @@
 {nixpack}:
 with nixpack.mkSources;
+let
+  cwgh = repo: rev: gh "composewell" repo rev;
+in
 {
 layers = [
 {
@@ -7,6 +10,9 @@ layers = [
     // { c2nix = ["--flag no-charts"];
          flags = ["--flags no-charts"];
        };
+
+  streamly-coreutils =
+    cwgh "streamly-coreutils" "9daf64da9868c8e6bed11d51bcff45bf6e0b0a93";
 }
 ];
 }
